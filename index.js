294dc1377a1b48158e7625ff0428e3dbad5ec196
Jatin <chaurasiyajatin68@gmail.com>
2024-02-22 09:28:07 +0530

    jatin

--- index.js
+++ index.js
@@ -40,3 +40,64 @@ window.onscroll = () =>{
 
     };
 
+
+
+
+
+
+
+
+
+
+
+
+
+    var swiper = new Swiper(".product-slider", {
+      loop: true,
+        spaceBetween: 20,
+
+        autoplay: {
+            delay:7500,
+            disableOnInteraction:false,
+        },
+        
+        breakpoints: {
+         0: {
+            slidesPerView: 2,
+            
+          },
+          768: {
+            slidesPerView: 4,
+      
+          },
+          1024: {
+            slidesPerView: 5,
+          
+          },
+        },
+      });
+
+      var swiper = new Swiper(".review-slider", {
+        loop: true,
+          spaceBetween: 20,
+  
+          autoplay: {
+              delay:7500,
+              disableOnInteraction:false,
+          },
+          
+          breakpoints: {
+           0: {
+              slidesPerView: 2,
+              
+            },
+            768: {
+              slidesPerView: 4,
+        
+            },
+            1024: {
+              slidesPerView: 5,
+            
+            },
+          },
+        });
